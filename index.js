97be050e76994e7b7d2b4a745f50e1c5179a24a4
aZul <fmdcosta.piano@gmail.com>
2023-11-24 20:27:52 +0000

    fix: Fix index.html and .js initialization

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import twgl from "twgl.js"
+import * as twgl from "./node_modules/twgl.js/dist/5.x/twgl-full.module.js"
+import config from "./star-config.json" assert {type: "json"}
+import {
+    vsDirect,
+    fsDirect
+} from "./shaders.js"
+
+const m4 = twgl.m4
+const primitives = twgl.primitives
 
 function main() {
+    const programInfo = twgl.createProgramInfo(gl, [vsDirect, fsDirect])
 
 }
+
+function initGL(gl) {}
+
+function render(step) {}
