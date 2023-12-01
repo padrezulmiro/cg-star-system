15a8b207225a74bea188d7231730278459e3d06b
enderesting <datendza@gmail.com>
2023-12-01 18:20:27 +0000

    added arrow key based panning (relative to world)

--- camera.js
+++ camera.js
@@ -5,11 +5,32 @@ cam_location - rotate around where the camera is
 THETA - x
 PHI - y
  */
-export function turnCamera(m4,cam_matrix, cam_location, THETA,PHI){
-
-    // move world matrix to origin
-    var m = m4.rotateY(m4.rotateX(cam_matrix, PHI),THETA)
+export function turnCamera(m4,cam_matrix, cam_pos, THETA,PHI){
+    // return it to origin
+    var cam_location = m4.translation(cam_pos); // translate vec3 to vec4
+    var m = m4.multiply(cam_matrix, m4.inverse(cam_location));
+    // apply rotation
+    m = m4.rotateX(m, PHI);
+    m = m4.rotateY(m,THETA);
+    // put it back to camera origin
+    m = m4.multiply(m, cam_location);
     return m;
-    // rotate?
-    // move back
+
+    //attempt 1
+    // // return it to origin
+    // var cam_location = m4.translation(cam_pos); // translate vec3 to vec4
+    // var m = m4.multiply(cam_matrix, m4.inverse(cam_location));
+    // // apply rotation
+    // m = m4.rotateX(m, PHI);
+    // m = m4.rotateY(m,THETA);
+    // // put it back to camera origin
+    // m = m4.multiply(m, cam_location);
+    // return m;
+}
+
+/*
+run after planet's position is ran.
+*/
+export function lookAtPlanet(m4, cam_matrix, ){
+
 }
--- index.js
+++ index.js
@@ -18,10 +18,15 @@ const bufferInfo = twgl.primitives.createSphereBufferInfo(gl,1,24,24);
 const uniforms = {};
 
 gl.clearColor(0, 0, 0.2, 1);  // background color
-requestAnimationFrame(render);
+
+// ========== INITIAL CAM config ==========
+var eye = [0, 0, -10];
+var target = [0, 0, 0];
+var up = [0, 1, 0];
+var camera = m4.lookAt(eye, target, up);
 
 // ========== MOUSE EVENTS ==========
-var AMORTIZATION = 0.95;
+// ref: http://www.webglacademy.com/courses.php?courses=0_1_20_2_3_4_23_5_6_7_10#4
 var drag = false;
 var THETA = 0, PHI = 0;
 
@@ -49,16 +54,65 @@ var mouseMove = function(e) {
   e.preventDefault();
 };
 
+
+
 CANVAS.addEventListener("mousedown", mouseDown, false);
 CANVAS.addEventListener("mouseup", mouseUp, false);
 CANVAS.addEventListener("mouseout", mouseUp, false);
 CANVAS.addEventListener("mousemove", mouseMove, false);
 
+// ========== KB EVENTS ==========
+var mv_dX = 0, mv_dZ = 0; //dX short for DYKES I <3 WOMEN
+// var u = [1, 0, 0]; //+X axis
+// var v = [0, 1, 0]; //+Y axis
+// var n = [0, 0, 1]; //+Z axis
+var AMORTIZATION = 0.5; //bit of a slowdown so movement isn't so immediate.
+var LOWER_BOUND = 0.01; //stops calculating at this bound
+var is_key_down = false;
+var keyDown = function(e){
+    is_key_down =true;
+    switch (e.keyCode) {
+        case 37: //left
+            // console.log("left");
+            mv_dX = -1;
+            break;
+        case 38: //up
+            // console.log("forward");
+            mv_dZ = -1;
+            break;
+        case 39: //right
+            // console.log("right");
+            mv_dX = 1;
+            break;
+        case 40: //down
+            // console.log("back");
+            mv_dZ = 1;
+            break;
+    }
+    // console.log("x:",mv_dX,"  z:",mv_dZ);
+};
 
+var keyUp = function(e){
+    is_key_down = false
+};
+
+document.addEventListener("keydown", keyDown, false);
+document.addEventListener("keyup", keyUp, false);
+
+
+// ========== FINALLY: BEGIN RENDER ==========
+requestAnimationFrame(render);
 
 function render(time) {
     // console.log('THETA:',THETA, 'PHI:', PHI);
-    
+    var movement = [mv_dX,0,mv_dZ];
+    console.log("movement:",movement, " key:", is_key_down);
+    if(!is_key_down){
+        if (Math.abs(mv_dX) <= LOWER_BOUND){mv_dX = 0};
+        if (Math.abs(mv_dZ) <= LOWER_BOUND){mv_dZ = 0};
+        mv_dX*=AMORTIZATION;
+        mv_dZ*=AMORTIZATION;
+    }
     twgl.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
@@ -69,18 +123,20 @@ function render(time) {
     const fov = 30 * Math.PI / 180;
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     const zNear = 0.5;
-    const zFar = 10;
+    const zFar = 100;
     const projection = m4.perspective(fov, aspect, zNear, zFar);
 
-    const eye = [0, 0, -10];
-    const target = [0, 0, 0];
-    const up = [0, 1, 0];
-    const camera = m4.lookAt(eye, target, up);
+    // eye[0] = mv_dX;
+    // eye[2] = mv_dZ;
+    camera = m4.translate(camera,movement);
+    // dst[12] = eye[0];
+    // dst[13] = eye[1];
+    // dst[14] = eye[2];
     const turned_camera = cam.turnCamera(m4,camera,eye,THETA,PHI); //turning camera based on mouse movement
     const view = m4.inverse(turned_camera);
 
     const viewProjection = m4.multiply(projection, view);
-    const world = m4.rotationY(time*0.001);//m4.identity();
+    const world = m4.identity();//m4.rotationY(time*0.001);
 
     uniforms.u_world = world;
     uniforms.u_worldViewProjection = m4.multiply(viewProjection, world);
@@ -95,4 +151,7 @@ function render(time) {
 
 function initGL(gl) {}
 
-// function render(step) {}
+// calculate the vector of target and eye
+// normalize it for the direction
+// multiply it with the arrow key directions of the frame.
+//
