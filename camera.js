08005c1e76541383eac6449565ee19cf881d179a
enderesting <datendza@gmail.com>
2023-12-02 16:44:34 +0000

    cleaned up, fixed consistency bugs

--- camera.js
+++ camera.js
@@ -42,17 +42,6 @@ export class Camera{
         this.zAxis = m4.transformDirection(m4.rotationX(PHI),this.zAxis);
         this.rt_dX = THETA;
         this.rt_dY = PHI;
-
-        // //VERSION 2: DIT
-        // // return it to origin
-        // var cam_location = m4.translation(cam_matrix,this.eye); // translate vec3 to vec4
-        // var m = m4.multiply(cam_matrix, m4.inverse(cam_location));
-        // // apply rotation
-        // m = m4.rotateX(m, PHI);
-        // m = m4.rotateY(m,THETA);
-        // // put it back to camera origin
-        // m = m4.multiply(m, cam_location);
-        // return m;
     }
 
     moveCameraByV(v){
@@ -65,54 +54,13 @@ export class Camera{
     moveCameraToTarget(eye,target,up){
         // eye = eye || this.eye;
         // target = target || this.target;
-        up = up ||this.up;
+        // up = up ||this.up;
         this.camera = m4.lookAt(eye,target,up);
-    }
-
-    getDirection(cam_matrix){
-        // var mat = m4.inverse(cam_matrix);
-        // console.log('cam_mat',cam_matrix,'inversed mat',mat);
-        var zAxis_world = m4.transformPoint(m4.inverse(cam_matrix),this.zAxis);
-        // console.log("zAxis",this.zAxis,"      zAxis_world",zAxis_world);
-        return zAxis_world;
-    }
-
-
-
-    v_CamToWorldSpace(cam_matrix, v){
-        var target_world = m4.transformPoint(cam_matrix,v);
-        var v_world = target_world - this.eye;
-
-        return v_world;
+        this.zAxis = v3.normalize(v3.subtract(eye, target));
+        this.xAxis = v3.normalize(v3.cross(up, this.zAxis));
+        this.yAxis = v3.normalize(v3.cross(this.zAxis, this.xAxis));
+        this.eye = eye;
+        this.target = target;
+        this.up = up;
     }
 }
-
-export function getCamAxises(){
-
-}
-
-
-// /*
-// cam_matrix - matrix for the camera
-// cam_location - rotate around where the camera is
-// THETA - x
-// PHI - y
-//  */
-// export function turnCamera(cam_matrix,THETA,PHI){
-//     var cam = m4.axisRotate(cam_matrix,this.xAxis,THETA);
-//     cam = m4.axisRotate(cam,this.yAxis,PHI);
-//     return cam;
-// }
-    // // return it to origin
-    // var cam_location = m4.translation(cam_pos); // translate vec3 to vec4
-    // var m = m4.multiply(cam_matrix, m4.inverse(cam_location));
-    // // apply rotation
-    // m = m4.rotateX(m, PHI);
-    // m = m4.rotateY(m,THETA);
-    // // put it back to camera origin
-    // m = m4.multiply(m, cam_location);
-    // return m;
-// /*
-// run after planet's position is ran.
-// */
-// export function lookAtPlanet(m4, cam_matrix, ){
--- index.js
+++ index.js
@@ -31,11 +31,9 @@ var camera = cameraConfig.camera;
 // ========== MOUSE EVENTS ==========
 // ref: http://www.webglacademy.com/courses.php?courses=0_1_20_2_3_4_23_5_6_7_10#4
 var drag = false;
-var THETA = 0, PHI = 0;
-
 var x_prev, y_prev;
-
 var rt_dX = 0, rt_dY = 0;
+
 var mouseDown = function(e) {
   drag = true;
   x_prev = e.pageX, y_prev = e.pageY;
@@ -51,13 +49,11 @@ var mouseMove = function(e) {
   if (!drag) return false;
     rt_dX = (e.pageX-x_prev) * 2 * Math.PI / CANVAS.width,
     rt_dY = (e.pageY-y_prev) * 2 * Math.PI / CANVAS.height;
-    THETA += rt_dX;
-    PHI += rt_dY;
+    // THETA += rt_dX;
+    // PHI += rt_dY;
     x_prev = e.pageX, y_prev = e.pageY;
     e.preventDefault();
 };
-
-
 
 CANVAS.addEventListener("mousedown", mouseDown, false);
 CANVAS.addEventListener("mouseup", mouseUp, false);
@@ -66,9 +62,6 @@ CANVAS.addEventListener("mousemove", mouseMove, false);
 
 // ========== KB EVENTS ==========
 var mv_dX = 0, mv_dZ = 0; //dX short for DYKES I <3 WOMEN
-// var u = [1, 0, 0]; //+X axis
-// var v = [0, 1, 0]; //+Y axis
-// var n = [0, 0, 1]; //+Z axis
 var AMORTIZATION = 0.5; //bit of a slowdown so movement isn't so immediate.
 var LOWER_BOUND = 0.01; //stops calculating at this bound
 var is_key_down = false;
@@ -76,19 +69,15 @@ var keyDown = function(e){
     is_key_down =true;
     switch (e.keyCode) {
         case 37: //left
-            // console.log("left");
             mv_dX = -1;
             break;
         case 38: //up
-            // console.log("forward");
             mv_dZ = -1;
             break;
         case 39: //right
-            // console.log("right");
             mv_dX = 1;
             break;
         case 40: //down
-            // console.log("back");
             mv_dZ = 1;
             break;
         case 48: //0
@@ -96,13 +85,12 @@ var keyDown = function(e){
             cameraConfig.moveCameraToTarget(eye,target,up);
             break;
         case 49: //1
-            //point at earth
+            //FIXME: point at earth
             break;
         case 50: //2
-            //point at sun
+            //FIXME: point at sun
             break;
     }
-    // console.log("x:",mv_dX,"  z:",mv_dZ);
 };
 
 var keyUp = function(e){
@@ -112,22 +100,10 @@ var keyUp = function(e){
 document.addEventListener("keydown", keyDown, false);
 document.addEventListener("keyup", keyUp, false);
 
-
 // ========== FINALLY: BEGIN RENDER ==========
 requestAnimationFrame(render);
 
 function render(time) {
-    //*********************************** to calculate movement
-    var movement = [mv_dX,0,mv_dZ];
-    // console.log("movement:",movement, " key:", is_key_down);
-    if(!is_key_down){
-        if (Math.abs(mv_dX) <= LOWER_BOUND){mv_dX = 0};
-        if (Math.abs(mv_dZ) <= LOWER_BOUND){mv_dZ = 0};
-        mv_dX*=AMORTIZATION;
-        mv_dZ*=AMORTIZATION;
-    }
-    //***********************************
-
     twgl.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
@@ -142,25 +118,17 @@ function render(time) {
     const projection = m4.perspective(fov, aspect, zNear, zFar);
 
 
-    //*********************************** camera calculations
-
-    // var pointing_at = v3.normalize(cameraConfig.getDirection(camera));
-    // // console.log('pointing',pointing_at);
-    // // console.log('pre-direction oriented movement:', movement);
-    // movement = v3.multiply(pointing_at,movement);//m4.transformDirection(pointing_at,movement);
-    // // // movement = m4.transformDirection(pointing_at,movement);
-    // console.log('movement:', movement);
-    // camera = m4.translate(camera,movement);
-    // // console.log('aftertranslation,',camera);
-
-    console.log('camera movement:', movement);
-    // var movement_world = cameraConfig.v_CamToWorldSpace(camera,movement);
-    // console.log('world movement:', movement_world);
-
-    cameraConfig.moveCameraByV(movement);//m4.translate(camera,movement_world);
-    // console.log('camera:', camera);
-
-    //rotation
+    //***********************************
+    //calculate movement
+    var movement = [mv_dX,0,mv_dZ];
+    if(!is_key_down){
+        if (Math.abs(mv_dX) <= LOWER_BOUND){mv_dX = 0};
+        if (Math.abs(mv_dZ) <= LOWER_BOUND){mv_dZ = 0};
+        mv_dX*=AMORTIZATION;
+        mv_dZ*=AMORTIZATION;
+    }
+    cameraConfig.moveCameraByV(movement); 
+    //calculate rotation
     cameraConfig.turnCamera(rt_dX,rt_dY); //turning camera based on mouse movement
     rt_dX = 0;
     rt_dY = 0;
@@ -182,8 +150,3 @@ function render(time) {
 }
 
 function initGL(gl) {}
-
-// calculate the vector of target and eye
-// normalize it for the direction
-// multiply it with the arrow key directions of the frame.
-//
