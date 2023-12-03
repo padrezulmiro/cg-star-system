8395412b9878ebda1ec929439c05fec0cc8ed672
enderesting <datendza@gmail.com>
2023-12-03 00:48:31 +0000

    bug fixed!

--- shaders.js
+++ shaders.js
@@ -3,23 +3,25 @@ uniform mat4 u_matrix;
 
 attribute vec4 position;
 
-attribute vec2 texCoord;
-varying vec2 v_texCoord;
+attribute vec2 texcoord;
+varying vec2 v_texcoord;
 
 void main()
 {
     gl_Position = u_matrix * position;
-    v_texCoord = texCoord;
+    v_texcoord = texcoord;
 }
 `
 
 export const fsDirect = `
 precision mediump float;
 
-varying vec2 v_texCoord;
+varying vec2 v_texcoord;
 uniform sampler2D u_texture;
 
 void main() {
-    gl_FragColor = texture2D(u_texture, v_texCoord);
+    gl_FragColor = texture2D(u_texture, v_texcoord);
+    //gl_FragColor = vec4(1,0,0,1);
+    // gl_FragColor = vec4(v_texcoord[0],v_texcoord[1],0,1);  // -------CHANGED-------------------------
 }
 `
